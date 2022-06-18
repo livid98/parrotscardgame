c5075744c598f987b38d6ac5e8d31f7d8d4e69bb
livid98 <liviad877@gmail.com>
2022-06-18 17:48:54 -0300

    agoravai

--- projeto.js
+++ projeto.js
@@ -1,35 +1,34 @@
 
-let numcartas=0;
 
-let gifs = ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot"]
+let gifs = ["bobrossparrot","explodyparrot","fiestaparrot","metalparrot","revertitparrot","tripletsparrot","unicornparrot"];
 
- function iniciar(){
- let numcartas = prompt("Com Quantas cartas você quer jogar?Apenas números pares e até 14.")
- numcartas = Number(numcartas);
- if(isNaN(Number(numcartas))===true || numcartas<4 || numcartas>14 || numcartas%2 !==0) {
- alert ("Número inválido. Siga os requisitos.");
- numcartas= 0;
- iniciar();
- }
+ function iniciar() {
+ numcartas = prompt("Com Quantas cartas você quer jogar?Apenas números pares e até 14.");
+  numcartas = Number(numcartas);
+  if(isNaN(Number(numcartas))===true || numcartas<4 || numcartas>14 || numcartas%2 !==0) {
+   alert ("Número inválido. Siga os requisitos.");
+    numcartas= 0;
+   iniciar();
+    }
+    addcartas ();
+   }  
+      
+  //  let gifsnum = [];
+  // for(let index=0; index<numcartas/2; index++){
+  //  gifsnum.push(gifs[index]);
+  //   gifsnum.push(gifs[index]);
+  // }
+//   gifsnum.sort(comparador);
 
-  let gifsnum = [];
-  for(let index=0; index<numcartas/2; index++){
-   gifsnum.push(gifs[index]);
-   gifsnum.push(gifs[index]);
+  function addcartas(){
+   for(let i=0; i<numcartas; i++){
+  let cartas = `<div class="frente">
+  <img src="img/${gifs[i]}.gif" alt="">
+ </div> <div class="tras">
+   <img src="img/${gifs[i]}.gif" alt="">
+  </div>` ; 
+   document.querySelector(".conteudo").innerHTML += cartas;
+
   }
-  gifsnum.sort(comparador);
-
- function addcartas(){
-  for(let i=0; i<numcartas; i++){
-  let cartas =  `<div class="carta">
-  <img src="img\${gifsnum[i]}.gif" alt="">
- </div>` ; 
-  document.querySelector(".conteudo").innerHTML += cartas
 
   }
-
-}
-
- }
-addcartas(numcartas);
- 
